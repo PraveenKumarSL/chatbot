5bc02ca052756f86b05cfff70fdcb2d1253104c1
PraveenKumarSL <shankarpk1319@gmail.com>
2025-07-16 15:32:39 +0530

    server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 from recommender import (
     classify_input,
     recommend_tools,
@@ -39,4 +40,5 @@ def chat():
         })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
